d40b4482aa32d6054b433b9d67a26a1c3bd831bf
Echoffee <light.runa@gmail.com>
2017-02-09 16:10:16 +0100

    Change app color theme

--- src/main/java/newera/myapplication/ui/view/CImageView.java
+++ src/main/java/newera/myapplication/ui/view/CImageView.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
+import newera.myapplication.R;
 import newera.myapplication.image.Image;
+
+import java.util.ResourceBundle;
 
 /**
  * Created by Emile Barjou-Suire on 09/02/2017.
@@ -22,7 +26,8 @@ public class CImageView extends View {
     @Override
     public void onDraw(Canvas canvas){
         if (image == null || !image.isEmpty()){
-            canvas.drawColor(Color.rgb(51,51,51));
+            canvas.drawColor(0x5E7078);
+            Log.i("INFO", "" + R.color.colorPrimary);
         } else {
             canvas.drawBitmap(image.getBitmap(), 0, 0, null);
         }
